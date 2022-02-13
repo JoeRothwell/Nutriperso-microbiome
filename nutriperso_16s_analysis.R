830224273b1f011c24bfd8882d0ca890a1d4ddad
Joe <joerothwell1@gmail.com>
2022-02-13 11:50:19 +0100

    Added 16s data analysis

---
+++ nutriperso_16s_analysis.R
@@ -0,0 +1,51 @@
+library(phyloseq)
+
+# Plots. see:
+# http://joey711.github.io/phyloseq/plot_bar-examples#enterotypes_dataset_examples
+# Default plot is abundance
+plot_bar(physeq1)
+plot_bar(physeq1, fill = "Phylum")
+
+# Group by a variable
+plot_bar(physeq1, x="casnutpkt", fill = "Phylum")
+plot_bar(physeq1, x="diabete_groupe", fill = "Phylum")
+
+# Facetted (doesn't work well)
+plot_bar(physeq1, fill = "Phylum", facet_grid = ~ diabete_groupe)
+
+# Heatmap (too complex)
+plot_heatmap(physeq1)
+
+# Plot tree with labelled diabetes group (too complex to read)
+plot_tree(physeq1, color="Phylum", shape="diabete_groupe", size="abundance")
+
+# Subsets
+ntaxa(physeq1)
+nsamples(physeq1)
+
+# Ordination plots
+# Plot of OTUs
+# http://joey711.github.io/phyloseq/plot_ordination-examples.html
+GP.ord <- ordinate(physeq1, "NMDS", "bray")
+plot_ordination(physeq1, GP.ord, type="taxa", color="Phylum", title="taxa")
+
+# Plot of samples
+plot_ordination(physeq1, GP.ord, type="samples", color="casnutpkt", shape="diabete_groupe")
+
+# Measures of alpha diversity
+plot_richness(physeq1, x="diabete_groupe")
+plot_richness(physeq1, x="casnutpkt")
+# Just selected measures
+plot_richness(physeq1, x="diabete_groupe", measures = c("Shannon", "Simpson"))
+
+# Plot a tree, mapping colour to case-control status or class
+plot_tree(physeq1, ladderize="left", color="casnutpkt")
+plot_tree(physeq1, ladderize="left", color="Class")
+
+plot_tree(physeq1, color="casnutpkt", ladderize="left") + coord_polar(theta="y")
+
+
+# Subset controls (need to change names)
+NPfr.cont <- subset_samples(NPfr, casnutpkt == "TEMOIN" & RUN == "RUN1")
+nsamples(NPfr.cont) #69 samples
+plot_bar(NPfr.cont, fill = "Phylum")
--- nutriperso_data_prep.R
+++ nutriperso_data_prep.R
@@ -1,21 +1,21 @@
-# Microbiome metadata
+# Preparation of 16s microbiome data and import into a phyloseq object
 library(readr)
-library(readxl)
-library(haven)
 library(janitor)
 library(tidyverse)
 
 # Case-control data (initially sent to INRA)
+library(readxl)
 dat <- read_xlsx("NP_RawSeq_ID.xlsx")
+
 # E3N variables
 #micro <- read_xls("microbiome_20210106.xls")
+library(haven)
 micro <- read_sas("nutriperso_20210304.sas7bdat")
 
-# Analysis of 16s tables. Two approaches are used, OTU and ASV. 
-# There are two tables per method: the distributions (.txt) and the taxonomic 
-# assignments for each OTU (see emails from Patricia, 18th May and 11th Oct)
-# Explanation of OTUs and ASVs at the beginning of this guide:
+# Preparation of 16s data.Two sets of data are provided, OTU and ASV. Explanation at:
 # https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html#abstract
+# There are two tables per method: the distributions (.txt) and the taxonomic assignments for each OTU 
+# See emails from Patricia, 18th May and 11th Oct
 
 # Start with OTU approach (distribution and taxonomies in separate tables)
 otus <- read.delim("NUTRIPERSO_assembled_350_TAB.txt")
@@ -28,15 +28,15 @@ taxmat <- tax %>% remove_constant() %>% select(-X2) %>% slice(-c(190, 507)) %>%
   filter(X5 > thr & X8 > thr & X11 > thr & X14 > thr & X17 > thr & X20 > thr) %>%
   select(-X5, -X8, -X11, -X14, -X17, -X20) %>% as.matrix
 
-nrow(taxmat) #632 OTUs remaining (or 584)
+nrow(taxmat) #632 OTUs remaining (or 584 for 0.7 threshold)
 colnames(taxmat) <- c("otu", "Domain", "Phylum", "Class", "Order", "Family", "Genus")
 
 # Get overlap between kept OTUs above threshold and all in otu matrix
 vec <- intersect(taxmat[, 1], otus$OTU)
 
-# Prepare data for phyloseq. See: http://joey711.github.io/phyloseq/import-data.html
+# Prepare otu and tax data for phyloseq. See: http://joey711.github.io/phyloseq/import-data.html
 # Also see https://www.yanh.org/2021/01/01/microbiome-r/
-# Also see Semi's script microbiome_nfbc1
+# Also see Semi's script microbiome_nfbc1.R
 
 # Remove OTU names and add back as rownames. Subset OTUs by their names in taxmat
 otumat <- otus[, -1]
@@ -47,6 +47,46 @@ otumat1 <- otumat[vec, ]
 taxmat <- taxmat[, -1]
 colnames(taxmat) <- c("Domain", "Phylum", "Class", "Order", "Family", "Genus")
 rownames(taxmat) <- rownames(otumat1)
+
+# Get phylogenetic tree
+# Some info here: https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html#construct_phylogenetic_tree
+# Also: https://rachaellappan.github.io/16S-analysis/index.html . From this site:
+# the UniFrac metric uses it for beta diversity calculations, as does the phylogenetic  
+# alpha diversity metric Faith’s PD
+
+# Use msa package for to align 16s sequences (uses command line tools clustal and muscle)
+# Only muscle worked
+library(msa)
+dat1 <- readDNAStringSet("NUTRIPERSO_assembled_350_OTU.fna")
+retained.otu <- dat1[rownames(otumat1)]
+otu.align <- msa(retained.otu, type = "dna", method = "Muscle")
+
+# Convert to a seqinr object
+otu.align1 <- msaConvert(otu.align, type = "seqinr::alignment")
+otu.dist <- dist.alignment(otu.align1, "identity")
+# Neighbour-joining tree estimation in ape
+otuTree <- nj(otu.dist)
+plot(otuTree)
+
+# Other things investigated but not used
+# Here is a random tree in ape:
+random_tree = rtree(ntaxa(physeq), rooted=TRUE, tip.label=taxa_names(physeq))
+plot(random_tree)
+
+# Read in the OTU sequences from the fna file with seqinr (not used)
+library(seqinr)
+seqs <- read.fasta("NUTRIPERSO_assembled_350_OTU.fna")
+
+# Phangorn produces more sophisticated trees
+library(phangorn)
+# Need to fit a model to the data and then extract $tree
+# Also see: https://fuzzyatelin.github.io/bioanth-stats/module-24/module-24.html
+# Also: https://bioinformatics.stackexchange.com/questions/7019/how-to-create-phylogenetic-trees-from-fasta-files-in-python-or-r/7021
+# Not sure how to do it this way
+
+# Reading in data with ape
+library(ape)
+dat <- read.dna("NUTRIPERSO_assembled_350_OTU.fna", format = "fasta")
 
 # Make phyloseq object using constructors like otu_table
 library(phyloseq)
@@ -69,96 +109,11 @@ sampledata <- sample_data(data.frame(sampdata1, row.names = sample_names(physeq)
 physeq1 <- merge_phyloseq(physeq, sampledata, otuTree)
 physeq1
 
-
-# Now just need the phylogenetic tree for downstream analysis and to add this to phyloseq
-# Some info here: https://bioconductor.org/help/course-materials/2017/BioC2017/Day1/Workshops/Microbiome/MicrobiomeWorkflowII.html#construct_phylogenetic_tree
-# Also: https://rachaellappan.github.io/16S-analysis/index.html . From this site:
-# the UniFrac metric uses it for beta diversity calculations, as does the phylogenetic  
-# alpha diversity metric Faith’s PD
-
-# Use msa package for to align 16s sequences (uses command line tools clustal and muscle)
-# Only muscle worked
-library(msa)
-dat1 <- readDNAStringSet("NUTRIPERSO_assembled_350_OTU.fna")
-retained.otu <- dat1[rownames(otumat1)]
-otu.align <- msa(retained.otu, type = "dna", method = "Muscle")
-
-# Convert to a seqinr object
-otu.align1 <- msaConvert(otu.align, type = "seqinr::alignment")
-otu.dist <- dist.alignment(otu.align1, "identity")
-# Neighbour-joining tree estimation in ape
-otuTree <- nj(otu.dist)
-plot(otuTree)
-
-
-# Other things investigated but not used
-# Here is a random tree in ape:
-random_tree = rtree(ntaxa(physeq), rooted=TRUE, tip.label=taxa_names(physeq))
-plot(random_tree)
-
-# Read in the OTU sequences from the fna file with seqinr (not used)
-library(seqinr)
-seqs <- read.fasta("NUTRIPERSO_assembled_350_OTU.fna")
-
-library(phangorn)
-# Need to fit a model to the data and then extract $tree
-# Also see: https://fuzzyatelin.github.io/bioanth-stats/module-24/module-24.html
-# Also: https://bioinformatics.stackexchange.com/questions/7019/how-to-create-phylogenetic-trees-from-fasta-files-in-python-or-r/7021
-# Not sure how to do it this way
-
-# Reading in data with ape
-library(ape)
-dat <- read.dna("NUTRIPERSO_assembled_350_OTU.fna", format = "fasta")
-
-# Use msa package for alignment (uses command line tools clustal and muscle)
-
-
-# Plots. see:
-# http://joey711.github.io/phyloseq/plot_bar-examples#enterotypes_dataset_examples
-# Default plot is abundance
-plot_bar(physeq1)
-plot_bar(physeq1, fill = "Phylum")
-
-# Group by a variable
-plot_bar(physeq1, x="casnutpkt", fill = "Phylum")
-plot_bar(physeq1, x="diabete_groupe", fill = "Phylum")
-
-# Facetted (doesn't work well)
-plot_bar(physeq1, fill = "Phylum", facet_grid = ~ diabete_groupe)
-
-# Subsets
-ntaxa(physeq1)
-nsamples(physeq1)
-
-# Pre-processing. Transform to relative abundances (scale 0-1), filter
-NPr  = transform_sample_counts(physeq1, function(x) x / sum(x) )
-NPfr = filter_taxa(NPr, function(x) mean(x) > 1e-5, TRUE)
-
-# Subset controls
-NPfr.cont <- subset_samples(NPfr, casnutpkt == "TEMOIN" & RUN == "RUN1")
-nsamples(NPfr.cont) #69 samples
-plot_bar(NPfr.cont, fill = "Phylum")
-
-
-
-# Import with mothur
-library(mothur)
-dat <- import_mothur(mothur_shared_file = "NUTRIPERSO_assembled_350_TAB.txt",
-                     mothur_constaxonomy_file = "NUTRIPERSO_assembled_350_ASV.tax")
-
-import("mothur", "NUTRIPERSO_assembled_350_TAB.txt")
-
 ### ASV approach. Data and taxonomies in the same table, with taxonomies at the end
 asvs <- read.delim("NUTRIPERSO_assembled_350_TAB_ASV.txt")
 tax <- asvs %>% select(OTU0:X1.0.5)
 
 #tax1 <- read_tsv("NUTRIPERSO_assembled_350_ASV.tax", col_names = F) 
-
-
-# Aligned sequences
-seqs <- read.delim("NUTRIPERSO_assembled_350_OTU.fna")
-taxmat <- tax1 %>% remove_constant() %>% 
-  select(-X1, -X5, -X8, -X11, -X14, -X16, -X17, -X19, -X20) %>% as.matrix
 
 # Initial 16S data (abandonned)
 #dat16s <- read_xlsx("RelAb_OTUs_Tab_NP_Lepage.xlsx")
