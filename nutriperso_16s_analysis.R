bca0d3d6545eefce369c3fb1a1ca35930465b617
Joe <joerothwell1@gmail.com>
2023-03-31 14:36:39 +0200

    Updates

--- nutriperso_16s_analysis.R
+++ nutriperso_16s_analysis.R
@@ -4,7 +4,7 @@ library(phyloseq)
 # http://joey711.github.io/phyloseq/plot_bar-examples#enterotypes_dataset_examples
 # Default plot is abundance
 plot_bar(physeq1)
-plot_bar(physeq1, fill = "Phylum") + theme(axis.labels.x = element_blank())
+plot_bar(physeq1, fill = "Phylum") #+ theme(axis.labels.x = element_blank())
 
 # Group by a variable
 plot_bar(physeq1, x="casnutpkt", fill = "Phylum")
@@ -31,13 +31,18 @@ GP.ord <- ordinate(physeq1, "NMDS", "bray")
 plot_ordination(physeq1, GP.ord, type="taxa", color="Phylum", title="taxa")
 
 # Plot of samples
+plot_ordination(physeq1, GP.ord, type="samples", color="casnutpkt") + 
+  theme_bw() + theme(legend.title = element_blank())
 plot_ordination(physeq1, GP.ord, type="samples", color="casnutpkt", shape="diabete_groupe")
 
 # Measures of alpha diversity
 plot_richness(physeq1, x="diabete_groupe")
 plot_richness(physeq1, x="casnutpkt", color = "casnutpkt")
 # Just selected measures
-plot_richness(physeq1, x="diabete_groupe", measures = c("Shannon", "Simpson"))
+plot_richness(physeq1, x="casnutpkt", color = "casnutpkt", measures = c("Shannon", "Simpson")) +
+  theme_bw() + theme(legend.title = element_blank(), axis.title.x = element_blank()) +
+  geom_point(colour = "white") + theme(legend.position="none") +
+  geom_jitter()
 
 # Plot a tree, mapping colour to case-control status or class
 plot_tree(physeq1, ladderize="left", color="casnutpkt")
