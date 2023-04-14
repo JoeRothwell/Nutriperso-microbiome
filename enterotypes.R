206938b77c1941c0fb4ab1f260743d6a8afa64d4
JoeRothwell <joerothwell1@gmail.com>
2023-04-14 23:14:52 +0200

    Added nutriperso enterotypes

--- enterotypes.R
+++ enterotypes.R
@@ -24,13 +24,13 @@ dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
     }
   }
   colnames -> colnames(resultsMatrix) -> rownames(resultsMatrix)
-  as.dist(resultsMatrix)->resultsMatrix
+  as.dist(resultsMatrix) -> resultsMatrix
   attr(resultsMatrix, "method") <- "dist"
   return(resultsMatrix) 
 }
 
 # Apply function to data
-data.dist=dist.JSD(dat)
+data.dist <- dist.JSD(dat)
 
 
 # Use partitioning around medoids (PAM) clustering to cluster the abundance profiles
@@ -71,11 +71,6 @@ plot(nclusters, type="h", xlab="k clusters", ylab="CH index",main="Optimal numbe
 obs.silhouette=mean(silhouette(data.cluster, data.dist)[,3])
 cat(obs.silhouette) #0.1899451
 
-
-
-
-
-
 #data=noise.removal(data, percent=0.01)
 data
 ## plot 1
@@ -90,7 +85,7 @@ dev.new()
 s.class(obs.pcoa$li, fac=as.factor(data.cluster), grid=F,sub="Principal coordiante analysis")
 
 
-# Using the bioconductor package
+# Using the mbOmic bioconductor package and data table
 library(BiocManager)
 BiocManager::install("mbOmic")
 library(mbOmic)
@@ -103,13 +98,58 @@ dat <- as.data.frame(dat$data+0.001)
 setDT(dat, keep.rownames = TRUE)
 dat
 
+# Convert to bSet object and do enterotyping
 dat <- bSet(b =  dat)
 res <- estimate_k(dat)
 res
-
 ret <- enterotyping(dat, res$verOptCluster) 
 ret
 
+
+### Get enterotypes for our Nutriperso data. Load phyloseq object
 library(phyloseq)
-BiocManager::install("phyloseq")
-nutrir  <- transform_sample_counts(nutri, function(x) x / sum(x) )
+
+# First transform OTU table to relative abundance
+nutri.rel  <- transform_sample_counts(nutri, function(x) x / sum(x) )
+otus <- otu_table(nutri.rel)
+data.dist <- dist.JSD(otus)
+
+# Get clusters
+data.cluster <- pam.clustering(data.dist, k=3)
+
+# Assess optimal number of clusters
+require(clusterSim)
+nclusters = index.G1(t(otus), data.cluster, d = data.dist, centrotypes = "medoids")
+
+# Try 1-20 clusters and plot optimal CH index
+nclusters=NULL
+
+for (k in 1:20) { 
+  if (k==1) {
+    nclusters[k]=NA 
+  } else {
+    data.cluster_temp=pam.clustering(data.dist, k)
+    nclusters[k]=index.G1(t(otus),data.cluster_temp,  d = data.dist,
+                          centrotypes = "medoids")
+  }
+}
+
+plot(nclusters, type="h", xlab="k clusters", ylab="CH index",main="Optimal number of clusters")
+data.cluster
+
+# Package method. Transform to relative abudance
+nutri.rel  <- transform_sample_counts(nutri, function(x) x / sum(x) )
+otus1 <- data.frame(otus)
+library(impute)
+otus1 <- impute.knn(as.matrix(otus1), k = 100)
+otus1 <- as.data.frame(otus1$data + 0.001) 
+setDT(otus1, keep.rownames = TRUE)
+
+otus1
+
+# Convert to bSet object and do enterotyping
+otus1 <- bSet(b = otus1)
+res <- estimate_k(otus1)
+res
+ret <- enterotyping(otus1, res$verOptCluster) 
+ret
