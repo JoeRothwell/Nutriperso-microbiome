e6e596ce3180ee1e07f3677073deceb92a707fbc
JoeRothwell <joerothwell1@gmail.com>
2023-04-17 17:34:55 +0200

    Added more code for Nutriperso data

--- enterotypes.R
+++ enterotypes.R
@@ -1,29 +1,29 @@
 # Calculation of enterotypes using different methods
 
 library(tidyverse)
-load("nutriperso_phyloseq.rda")
 # Example of how to calculate enterotypes from https://enterotype.embl.de/enterotypes.html
 
-# Get table of relative abundances (all samples add up to 1)
-dat <- read.table(url("https://enterotype.embl.de/MetaHIT_SangerSamples.genus.txt"), header=T, row.names=1, dec=".", sep="\t")
+# Get table of relative abundances from https://enterotype.embl.de/MetaHIT_SangerSamples.genus.txt 
+# all samples add up to 1
+dat <- read.table("MetaHIT_SangerSamples.genus.txt", header=T, row.names=1, dec=".", sep="\t")
 dat <- dat[-1, ]
 
 # Function to determine the Jensen-Shannon Divergence, a distance metric
 
-dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
-  KLD <- function(x,y) sum(x *log(x/y))
-  JSD <- function(x,y) sqrt(0.5 * KLD(x, (x+y)/2) + 0.5 * KLD(y, (x+y)/2))
+dist.JSD <- function(inMatrix, pseudocount = 0.000001, ...) {
+  KLD <- function(x, y) sum(x * log(x/y))
+  JSD <- function(x, y) sqrt(0.5 * KLD(x, (x+y)/2) + 0.5 * KLD(y, (x+y)/2))
   matrixColSize <- length(colnames(inMatrix))
   matrixRowSize <- length(rownames(inMatrix))
   colnames <- colnames(inMatrix)
   resultsMatrix <- matrix(0, matrixColSize, matrixColSize)
   
-  inMatrix = apply(inMatrix,1:2,function(x) ifelse (x==0,pseudocount,x))
+  inMatrix = apply(inMatrix, 1:2, function(x) ifelse (x == 0, pseudocount, x))
   
   for(i in 1:matrixColSize) {
     for(j in 1:matrixColSize) { 
-      resultsMatrix[i,j]=JSD(as.vector(inMatrix[,i]),
-                             as.vector(inMatrix[,j]))
+      resultsMatrix[i, j] <- JSD(as.vector(inMatrix[ ,i]),
+                             as.vector(inMatrix[ ,j]))
     }
   }
   colnames -> colnames(resultsMatrix) -> rownames(resultsMatrix)
@@ -35,58 +35,112 @@ dist.JSD <- function(inMatrix, pseudocount=0.000001, ...) {
 # Apply function to data
 data.dist <- dist.JSD(dat)
 
-
 # Use partitioning around medoids (PAM) clustering to cluster the abundance profiles
 # Similar to k-means but supports any distance measure
+# x is a distance matrix and k the number of clusters
 
-pam.clustering=function(x,k) { # x is a distance matrix and k the number of clusters
-  require(cluster)
-  cluster = as.vector(pam(as.dist(x), k, diss=TRUE)$clustering)
-  return(cluster)
+library(cluster)
+
+pam.clustering <- function(x, k) {
+  clust <- as.vector(pam(as.dist(x), k, diss = TRUE)$clustering)
+  return(clust)
 }
 
 # Get clusters
-data.cluster <- pam.clustering(data.dist, k=3)
+data.cluster <- pam.clustering(data.dist, k = 3)
 
 # Assess optimal number of clusters
 require(clusterSim)
-nclusters = index.G1(t(dat), data.cluster, d = data.dist, centrotypes = "medoids")
+nclusters <- index.G1(t(dat), data.cluster, d = data.dist, centrotypes = "medoids")
 
 # Try 1-20 clusters and plot optimal CH index
-nclusters=NULL
+nclusters <- NULL
 
 for (k in 1:20) { 
-  if (k==1) {
-    nclusters[k]=NA 
+  if (k == 1) {
+    nclusters[k] = NA 
   } else {
-    data.cluster_temp=pam.clustering(data.dist, k)
-    nclusters[k]=index.G1(t(dat),data.cluster_temp,  d = data.dist,
-                          centrotypes = "medoids")
+    data.cluster_temp <- pam.clustering(data.dist, k)
+    nclusters[k] <- index.G1(t(dat),data.cluster_temp,  d = data.dist, centrotypes = "medoids")
   }
 }
 
-plot(nclusters, type="h", xlab="k clusters", ylab="CH index",main="Optimal number of clusters")
+plot(nclusters, type="h", xlab="k clusters", ylab="CH index", main="Optimal number of clusters")
 
 # Optimal cluster number is 3
 
 # Cluster validation
 
-obs.silhouette=mean(silhouette(data.cluster, data.dist)[,3])
+obs.silhouette <- mean(silhouette(data.cluster, data.dist)[ , 3])
 cat(obs.silhouette) #0.1899451
 
-#data=noise.removal(data, percent=0.01)
-data
-## plot 1
-obs.pca <- dudi.pca(data.frame(t(dat)), scannf=F, nf=10)
-obs.bet <- bca(obs.pca, fac=as.factor(data.cluster), scannf=F, nf=k-1) 
-dev.new()
-s.class(obs.bet$ls, fac=as.factor(data.cluster), grid=F,sub="Between-class analysis")
+dat <- noise.removal(dat, percent=0.01)
 
-#plot 2
-obs.pcoa <- dudi.pco(data.dist, scannf=F, nf=3)
-dev.new()
-s.class(obs.pcoa$li, fac=as.factor(data.cluster), grid=F,sub="Principal coordiante analysis")
+## Multi-dimentional scaling plots to visualise enterotypes
+## Between class analysis (plot 1)
+library(ade4)
+obs.pca <- dudi.pca(data.frame(t(dat)), scannf = F, nf = 10)
+obs.bet <- bca(obs.pca, fac = as.factor(data.cluster), scannf = F, nf = k-1) 
+#dev.new()
+s.class(obs.bet$ls, fac = as.factor(data.cluster), grid = F, sub = "Between-class analysis")
 
+# Principal coordinate analysis (plot 2)
+obs.pcoa <- dudi.pco(data.dist, scannf = F, nf = 3)
+#dev.new()
+s.class(obs.pcoa$li, fac = as.factor(data.cluster), grid = F, sub = "Principal coordinate analysis")
+
+
+#### Nutriperso data
+
+load("nutriperso_phyloseq.rda")
+nutri.rel  <- transform_sample_counts(nutri, function(x) x / sum(x) )
+otus <- otu_table(nutri.rel)
+
+data.dist <- dist.JSD(otus)
+
+# Get clusters (need to specify how many)
+data.cluster <- pam.clustering(data.dist, k = 3)
+
+# Assess optimal number of clusters
+library(clusterSim)
+nclusters <- index.G1(t(otus), data.cluster, d = data.dist, centrotypes = "medoids")
+
+# Try 1-20 clusters and plot optimal CH index
+nclusters <- NULL
+
+for (k in 1:20) { 
+  if (k == 1) {
+    nclusters[k] = NA 
+  } else {
+    data.cluster_temp <- pam.clustering(data.dist, k)
+    nclusters[k] <- index.G1(t(otus), data.cluster_temp, d = data.dist, centrotypes = "medoids")
+  }
+}
+
+plot(nclusters, type="h", xlab="k clusters", ylab="CH index", main="Optimal number of clusters")
+# Optimal number of clusters is 2
+
+data.cluster <- pam.clustering(data.dist, k = 2)
+
+# Cluster validation
+
+obs.silhouette <- mean(silhouette(data.cluster, data.dist)[ , 3])
+cat(obs.silhouette) #0.1899451
+
+otus <- noise.removal(otus, percent=0.01)
+
+## Multi-dimentional scaling plots to visualise enterotypes
+## Between class analysis (plot 1)
+library(ade4)
+obs.pca <- dudi.pca(data.frame(t(otus)), scannf = F, nf = 10)
+obs.bet <- bca(obs.pca, fac = as.factor(data.cluster), scannf = F, nf = k-1) 
+#dev.new()
+s.class(obs.bet$ls, fac = as.factor(data.cluster), grid = F, sub = "Between-class analysis")
+
+# Principal coordinate analysis (plot 2)
+obs.pcoa <- dudi.pco(data.dist, scannf = F, nf = 3)
+#dev.new()
+s.class(obs.pcoa$li, fac = as.factor(data.cluster), grid = F, sub = "Principal coordinate analysis")
 
 # Using the mbOmic bioconductor package and data table
 library(BiocManager)
@@ -95,14 +149,16 @@ library(mbOmic)
 library(data.table)
 
 # From https://bioconductor.org/packages/release/bioc/vignettes/mbOmic/inst/doc/enterotyping.html
-dat <- read.delim('http://enterotypes.org/ref_samples_abundance_MetaHIT.txt')
+# Data at http://enterotypes.org/ref_samples_abundance_MetaHIT.txt
+
+dat <- read.delim("ref_samples_abundance_MetaHIT.txt")
 dat <- impute::impute.knn(as.matrix(dat), k = 100)
-dat <- as.data.frame(dat$data+0.001) 
+dat <- as.data.frame(dat$data + 0.001) 
 setDT(dat, keep.rownames = TRUE)
 dat
 
 # Convert to bSet object and do enterotyping
-dat <- bSet(b =  dat)
+dat <- bSet(b = dat)
 res <- estimate_k(dat)
 res
 ret <- enterotyping(dat, res$verOptCluster) 
@@ -128,11 +184,11 @@ nclusters = index.G1(t(otus), data.cluster, d = data.dist, centrotypes = "medoid
 nclusters=NULL
 
 for (k in 1:20) { 
-  if (k==1) {
-    nclusters[k]=NA 
+  if (k == 1) {
+    nclusters[k] = NA 
   } else {
-    data.cluster_temp=pam.clustering(data.dist, k)
-    nclusters[k]=index.G1(t(otus),data.cluster_temp,  d = data.dist,
+    data.cluster_temp = pam.clustering(data.dist, k)
+    nclusters[k] = index.G1(t(otus), data.cluster_temp, d = data.dist,
                           centrotypes = "medoids")
   }
 }
@@ -158,11 +214,20 @@ ret <- enterotyping(otus1, res$verOptCluster)
 ret
 
 ### With phyloseq? Has distance methods
+# https://joey711.github.io/phyloseq/distance.html
 # Get the JSD on the phyloseq object
 nutri.rel  <- transform_sample_counts(nutri, function(x) x / sum(x) )
-jsdist <- phyloseq::distance(nutri.rel, method="jsd")
-# or
-# JSD(physeq)
+jsdist <- phyloseq::distance(nutri.rel, method="jsd") # or JSD(physeq)
+# Get clusters
+clust <- as.vector(pam(jsdist, 3, diss = TRUE)$clustering)
+clust <- pam(jsdist, 3, diss = TRUE)$clustering
+
+
+jsMDS <- ordinate(nutri, "MDS", distance = jsdist)
+p <- plot_ordination(nutri, jsMDS, color="casnutpkt", shape = clust)
+
+
+
 
 # Get clusters
 data.cluster <- pam.clustering(jsdist, k=3)
