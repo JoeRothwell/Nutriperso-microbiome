d071aea0fb779a7c251142903c00f9bac45ce773
JoeRothwell <joerothwell1@gmail.com>
2023-07-20 17:08:13 +0200

    Updates

--- nutriperso_16s_analysis.R
+++ nutriperso_16s_analysis.R
@@ -74,5 +74,72 @@ plot_ordination(nutri, ord, type="samples", color="casnutpkt", shape="diabete_gr
 # Can also plot ordination of OTUs
 plot_ordination(nutri, ord, type="taxa", color="Phylum", title="taxa") + theme_bw() +
   geom_hline(yintercept = 0, linetype = "dashed") + geom_vline(xintercept = 0, linetype = "dashed") 
+
+
+
+# Get enterotypes (for full detail, see enterotypes.R)
+load("nutriperso_phyloseq.rda")
+nutri.rel  <- transform_sample_counts(nutri, function(x) x / sum(x) )
+otus <- otu_table(nutri.rel)
+
+data.dist <- dist.JSD(otus)
+data.dist1 <- phyloseq::distance(nutri.rel, method="jsd")
+
+# Do a test run on our dataset with number of clusters set to 3
+data.cluster <- pam.clustering(data.dist, k = 3)
+
+# Assess optimal number of clusters
+library(clusterSim)
+nclusters <- index.G1(t(otus), data.cluster, d = data.dist, centrotypes = "medoids")
+
+# Try 1-20 clusters and plot optimal CH index
+nclusters <- NULL
+
+for (k in 1:20) { 
+  if (k == 1) {
+    nclusters[k] = NA 
+  } else {
+    data.cluster_temp <- pam.clustering(data.dist, k)
+    nclusters[k] <- index.G1(t(otus), data.cluster_temp, d = data.dist, centrotypes = "medoids")
+  }
+}
+
+plot(nclusters, type="h", xlab="k clusters", ylab="CH index", main="Optimal number of clusters")
+
+# Optimal number of clusters is 2; reset to 2
+data.cluster <- pam.clustering(data.dist, k = 2)
+
+# Cluster validation (index 3 is the sil_width for each obs)
+obs.silhouette <- mean(silhouette(data.cluster, data.dist)[ , 3])
+cat(obs.silhouette) #0.1209738 (acceptable)
+
+# Remove low abundance genera
+noise.removal <- function(dataframe, percent=0.01, top=NULL){
+  dataframe -> Matrix
+  bigones <- rowSums(Matrix) * 100 / (sum(rowSums(Matrix))) > percent 
+  Matrix_1 <- Matrix[bigones,]
+  print(percent)
+  return(Matrix_1)
+}
+
+otus <- noise.removal(otus, percent=0.01)
+
+
+## Multi-dimentional scaling plots to visualise enterotypes
+## Get data for BCA and PCA
+library(ade4)
+obs.pca <- dudi.pca(data.frame(t(otus)), scannf = F, nf = 10)
+obs.bet <- bca(obs.pca, fac = as.factor(data.cluster), scannf = F, nf = k-1) 
+
+## Between class analysis (plot 1)
+dev.new()
+s.class(obs.bet$ls, fac = as.factor(data.cluster), grid = F, sub = "Between-class analysis")
+# Doesn't work with only 2 clusters
+
+# Principal coordinate analysis (plot 2)
+obs.pcoa <- dudi.pco(data.dist, scannf = F, nf = 3)
+#dev.new()
+s.class(obs.pcoa$li, fac = as.factor(data.cluster), grid = F, sub = "Principal coordinate analysis")
+
   
 
